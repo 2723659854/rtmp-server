97395417d619ea070e076c5e23975159ebb9e814
songshu <2723659854@qq.com>
2025-12-29 11:55:31 +0800

    测试

--- server.php
+++ server.php
@@ -11,11 +11,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 /** 获取服务实例 */
 $server = \Root\Io\RtmpDemo::instance();
 /** 设置rtmp通信端口 可以自行修改 默认1935 */
-$server->rtmpPort = 1935;
+$server->rtmpPort = 8002;
 /** 设置flv通信端口 可以自行修改 默认8501 */
-$server->flvPort = 9501;
+$server->flvPort = 8004;
 /** hls通信端口 可以自行修改 默认80  */
-$server->webPort = 8000;
+$server->webPort = 8005;
 /** 启动服务 */
 $server->start();
 
