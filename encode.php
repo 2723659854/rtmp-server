be30e7ee55b95066f9616df1ec9b3d8904509a57
songshu <2723659854@qq.com>
2026-06-13 00:56:32 +0800

    修复web推流音视频不同步的问题

--- encode.php
+++ encode.php
@@ -2,8 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 ini_set('memory_limit', '512M');
-$file = __DIR__."/a.flv";
-
+$file = __DIR__."/test.flv";
+//本次修复后，检查生成的flv没有看到错误，使用ffmpeg转码mp4可以播放
+//D:\php\rtmp_server\flv\a\b>ffmpeg -v error -i a.flv -f null -
+//
+//D:\php\rtmp_server\flv\a\b>ffprobe -v error -show_error a.flv
 
 echo "=== 示例1: flv静态文件切片fMP4并合并为mp4文件 ===\n";
 $outputDir1 = __DIR__."/encode";
