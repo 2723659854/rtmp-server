dfdb67ea0649be3d9be35e7f72af50ef3315ef68
songshu <2723659854@qq.com>
2026-06-29 21:07:32 +0800

    调整启动文件

--- server.php
+++ server.php
@@ -1,31 +1,38 @@
 <?php
+
 // 检查PHP版本是否小于8.1
 if (version_compare(PHP_VERSION, '8.1.0', '<')) {
-    // 输出错误信息到标准错误（STDERR）
     fwrite(STDERR, "错误：此脚本需要PHP 8.1或更高版本，当前版本为 " . PHP_VERSION . "\n");
-    // 退出脚本并返回错误码1（表示一般错误）
     exit(1);
 }
+
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__."/config/app.php";
 ini_set('memory_limit', '2048M');
 
-/** 是否开启hls协议 false表示关闭，true表示开启 */
-define('FLV_TO_HLS',true);
-/** 是否录屏mp4 ， false表示关闭，true表示开启 */
-define('FLV_TO_MP4',false);
-/** 是否开启flv录屏 ， false表示关闭，true表示开启 */
-define('FLV_TO_RECORD',false);
-/** 是否开启flv推流到远程服务器 ， false表示关闭，true表示开启 */
-define('FLV_TO_PUSH',true);
+
 
 /** 获取服务实例 */
 $server = \Root\Io\RtmpDemo::instance();
-/** 设置rtmp通信端口 可以自行修改 默认1935 */
-$server->rtmpPort = 1935;
-/** 设置flv通信端口 可以自行修改 默认8501 */
-$server->flvPort = 8501;
-/** hls通信端口 可以自行修改 默认80  */
+$server->rtmpPort = BASE_RTMP_PORT;
+$server->flvPort = BASE_FLV_PORT;
 $server->webPort = 80;
-/** 启动服务 */
-$server->start();
 
+// 检测运行环境
+$isLinux = DIRECTORY_SEPARATOR === '/';
+$isWindows = !$isLinux;
+
+// 多进程启动
+if (ENABLE_MULTI_PROCESS) {
+    if ($isLinux && extension_loaded('pcntl')) {
+        startWithPcntl($server);
+    } elseif ($isWindows) {
+        startWithProcOpen($server);
+    } else {
+        fwrite(STDERR, "警告：当前环境不支持多进程，将以单进程模式启动\n");
+        $server->start();
+    }
+} else {
+    // 单进程模式
+    $server->start();
+}
